90ed3ca37b9acb935478fd0e74de64e2ad1ed12b
ikaub <dimanovikov22@gmail.com>
2020-06-26 17:32:57 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
---
+++ screens/CocktailsScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {View, Text} from "react-native";
+
+const CocktailsScreen = () => {
+    return (
+        <View>
+            <Text>Cocktails Screen</Text>
+        </View>
+    );
+}
+
+export default CocktailsScreen;
